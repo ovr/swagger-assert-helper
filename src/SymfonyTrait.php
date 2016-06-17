ef98f524535e81b305356cd1bbbd5a7d00121e0c
Dmitry Patsura <talk@dmtry.me>
2016-06-17 19:08:34 +0900

    Implement SymfonyTrait to create Request in easy way

---
+++ src/SymfonyTrait.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @author Patsura Dmitry https://github.com/ovr <talk@dmtry.me>
+ */
+
+namespace Ovr\Swagger;
+
+use Swagger\Annotations\Operation;
+use Symfony\Component\HttpFoundation\Request;
+
+trait SymfonyTrait
+{
+    /**
+     * Prepare a Symfony Request by Operation with $parameters
+     *
+     * @param Operation $operation
+     * @param array $parameters
+     * @return Request
+     */
+    public function makeRequestByOperation(Operation $operation, array $parameters = [])
+    {
+        $request = new Request();
+
+        $path = $operation->path;
+
+        if ($parameters) {
+            foreach ($parameters as $key => $value) {
+                $path = str_replace('{' . $key . '}', $value, $path);
+            }
+        }
+
+        $request->server->set('REQUEST_URI', $path);
+        $request->server->set('REQUEST_METHOD', $operation->method);
+
+        return $request;
+    }
+}
