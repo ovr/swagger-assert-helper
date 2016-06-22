cba259ff2cfa47e1e8c4d5f871ca81e6653098e5
Dmitry Patsura <talk@dmtry.me>
2016-06-22 22:05:36 +0900

    [Example] More actions on UserController

--- examples/api/controller/UserController.php
+++ examples/api/controller/UserController.php
@@ -33,4 +33,84 @@ class UserController
     {
 
     }
+
+    /**
+     * @SWG\Post(
+     * 		tags={"User"},
+     * 		path="/user",
+     * 		operationId="createUserById",
+     * 		summary="Find user by $id",
+     * 		@SWG\Response(
+     * 			response=200,
+     * 			description="success",
+     * 			@SWG\Schema(ref="#/definitions/UserResponse")
+     * 		),
+     * 		@SWG\Response(
+     * 			response=404,
+     * 			description="Not found"
+     * 		)
+     * )
+     */
+    public function createAction()
+    {
+
+    }
+    
+    /**
+     * @SWG\Put(
+     * 		tags={"User"},
+     * 		path="/user/{id}",
+     * 		operationId="deleteUserById",
+     * 		summary="Find user by $id",
+     * 		@SWG\Parameter(
+     * 			name="id",
+     * 			description="$id of the specified",
+     * 			in="path",
+     * 			required=true,
+     * 			type="string"
+     * 		),
+     * 		@SWG\Response(
+     * 			response=200,
+     * 			description="success",
+     * 			@SWG\Schema(ref="#/definitions/UserResponse")
+     * 		),
+     * 		@SWG\Response(
+     * 			response=404,
+     * 			description="Not found"
+     * 		)
+     * )
+     */
+    public function updateAction()
+    {
+
+    }
+
+    /**
+     * @SWG\Delete(
+     * 		tags={"User"},
+     * 		path="/user/{id}",
+     * 		operationId="deleteUserById",
+     * 		summary="Find user by $id",
+     * 		@SWG\Parameter(
+     * 			name="id",
+     * 			description="$id of the specified",
+     * 			in="path",
+     * 			required=true,
+     * 			type="string"
+     * 		),
+     * 		@SWG\Response(
+     * 			response=200,
+     * 			description="success",
+     * 			@SWG\Schema(ref="#/definitions/UserResponse")
+     * 		),
+     * 		@SWG\Response(
+     * 			response=404,
+     * 			description="Not found"
+     * 		)
+     * )
+     */
+    public function deleteAction()
+    {
+
+    }
 }
