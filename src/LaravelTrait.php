36174f9a18fa4a148a6dfc1e5aa09546a80b0e77
Dmitry Patsura <talk@dmtry.me>
2016-06-22 22:04:38 +0900

    Implement support for making Laravel HTTP Request

---
+++ src/LaravelTrait.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * @author Patsura Dmitry https://github.com/ovr <talk@dmtry.me>
+ */
+
+namespace Ovr\Swagger;
+
+use Illuminate\Http\Request;
+use InvalidArgumentException;
+use RuntimeException;
+use Swagger\Annotations\Operation;
+
+trait LaravelTrait
+{
+    /**
+     * Prepare a Symfony Request by Operation with $parameters
+     *
+     * @param Operation $operation
+     * @param array $parameters
+     * @param bool $skipRequired
+     * @return Request
+     * @throws \InvalidArgumentException
+     * @throws \RuntimeException
+     */
+    public function makeRequestByOperation(Operation $operation, array $parameters = [], $skipRequired = false)
+    {
+        $request = new Request();
+
+        $path = $operation->path;
+
+        if ($operation->parameters) {
+            foreach ($operation->parameters as $parameter) {
+                if (isset($parameters[$parameter->name])) {
+                    switch ($parameter->in) {
+                        case 'path':
+                            $path = str_replace('{' . $parameter->name . '}', $parameters[$parameter->name], $path);
+                            break;
+                        case 'header':
+                            $request->headers->set($parameter->name, $parameters[$parameter->name]);
+                            break;
+                        case 'query':
+                            $request->query->set($parameter->name, $parameters[$parameter->name]);
+                            break;
+                        case 'formData':
+                            $request->request->set($parameter->name, $parameters[$parameter->name]);
+                            break;
+                        default:
+                            throw new RuntimeException(
+                                sprintf(
+                                    'Parameter "%s" with ->in = "%s" is not supported',
+                                    $parameter->parameter,
+                                    $parameter->in
+                                )
+                            );
+                    }
+                } elseif ($parameter->required && !$skipRequired) {
+                    throw new InvalidArgumentException(
+                        sprintf(
+                            'Parameter "%s" is required, please pass value for this in $parameters',
+                            $parameter->name
+                        )
+                    );
+                }
+            }
+        }
+
+        $request->server->set('REQUEST_URI', $path);
+        $request->server->set('REQUEST_METHOD', $operation->method);
+
+        return $request;
+    }
+}
---
+++ tests/TraitHelpers/LaravelTraitTest.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @author Patsura Dmitry https://github.com/ovr <talk@dmtry.me>
+ */
+
+namespace Tests\TraitHelpers;
+
+use Symfony\Component\HttpFoundation\Request;
+
+class LaravelTraitTest extends \PHPUnit_Framework_TestCase
+{
+    use \Ovr\Swagger\LaravelTrait;
+
+    protected function getSwaggerWrapper()
+    {
+        return new \Ovr\Swagger\SwaggerWrapper(
+            \Swagger\scan(
+                __DIR__ . '/../../examples'
+            )
+        );
+    }
+
+    public function testHelper()
+    {
+        $operation = $this->getSwaggerWrapper()->getOperationByName('getUserById');
+
+        $request = $this->makeRequestByOperation($operation, ['id' => 1], false);
+        parent::assertInstanceOf(Request::class, $request);
+        parent::assertSame(Request::METHOD_GET, $request->getMethod());
+        parent::assertSame('/v1/user/1', $request->getRequestUri());
+    }
+}
