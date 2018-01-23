5050fba8b54aaa34139f9660db5600dda32bf6f3
Dmitry Patsura <talk@dmtry.me>
2018-01-24 00:10:20 +0900

    Refactoring(Symfony): Use hydrateRequestByOperation

--- src/SymfonyTrait.php
+++ src/SymfonyTrait.php
@@ -8,6 +8,7 @@ namespace Ovr\Swagger;
 use InvalidArgumentException;
 use RuntimeException;
 use Swagger\Annotations\Operation;
+use Swagger\Annotations\Parameter;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -26,73 +27,37 @@ trait SymfonyTrait
     public function makeRequestByOperation(Operation $operation, array $parameters = [], $options = 0)
     {
         $request = new Request();
-
         $path = $operation->path;
 
-        if ($operation->parameters) {
-            foreach ($operation->parameters as $parameter) {
-                if (array_key_exists($parameter->name, $parameters)) {
-                    $parameterValue = $parameters[$parameter->name];
-                    if ($parameter->enum && !($options & SwaggerWrapper::SKIP_ENUM_CHECK)) {
-                        if (!in_array($parameterValue, $parameter->enum)) {
-                            throw new InvalidArgumentException(
-                                sprintf(
-                                    'Parameter "%s" has enum {"%s"}, but value "%s"',
-                                    $parameter->name,
-                                    implode('"|"', $parameter->enum),
-                                    $parameterValue
-                                )
-                            );
-                        }
-                    }
-
-                    switch ($parameter->in) {
-                        case 'path':
-                            $path = str_replace('{' . $parameter->name . '}', $parameterValue, $path);
-                            break;
-                        case 'header':
-                            $request->headers->set($parameter->name, $parameterValue);
-                            break;
-                        case 'query':
-                            $request->query->set($parameter->name, $parameterValue);
-                            break;
-                        case 'formData':
-                            $request->request->set($parameter->name, $parameterValue);
-                            break;
-                        default:
-                            throw new RuntimeException(
-                                sprintf(
-                                    'Parameter "%s" with ->in = "%s" is not supported',
-                                    $parameter->parameter,
-                                    $parameter->in
-                                )
-                            );
-                    }
-
-                    unset($parameters[$parameter->name]);
-                } elseif ($parameter->required && !($options & SwaggerWrapper::SKIP_REQUIRED)) {
-                    throw new InvalidArgumentException(
-                        sprintf(
-                            'Parameter "%s" is required, please pass value for this in $parameters',
-                            $parameter->name
-                        )
-                    );
+        SwaggerWrapper::hydrateRequestByOperation(
+            $operation,
+            function (Parameter $parameter, $value) use ($request, &$path) {
+                switch ($parameter->in) {
+                    case 'path':
+                        $path = str_replace('{' . $parameter->name . '}', $value, $path);
+                        break;
+                    case 'header':
+                        $request->headers->set($parameter->name, $value);
+                        break;
+                    case 'query':
+                        $request->query->set($parameter->name, $value);
+                        break;
+                    case 'formData':
+                        $request->request->set($parameter->name, $value);
+                        break;
+                    default:
+                        throw new RuntimeException(
+                            sprintf(
+                                'Parameter "%s" with ->in = "%s" is not supported',
+                                $parameter->parameter,
+                                $parameter->in
+                            )
+                        );
                 }
-            }
-        } elseif ($parameters) {
-            throw new InvalidArgumentException(
-                sprintf(
-                    'Operation does not have parameters, but you pass %d parameter(s)',
-                    count($parameters)
-                )
-            );
-        }
-
-        foreach ($parameters as $parameter => $value) {
-            throw new RuntimeException(
-                "Parameter '{$parameter}' passed, but does not exist in request definition (swagger)"
-            );
-        }
+            },
+            $parameters,
+            $options
+        );
 
         $request->server->set('REMOTE_ADDR', '8.8.8.8');
         $request->server->set('REQUEST_URI', $path);
